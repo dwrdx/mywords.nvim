24505529192c08e51b6b8281702e7119dba64ae2
Edward XU <edward.xu@hotmail.com>
2021-12-11 22:54:58 +0800

    support guibg

--- lua/mywords.lua
+++ lua/mywords.lua
@@ -3,34 +3,43 @@ local cmd = vim.cmd
 local fn = vim.fn
 local colors = vim.g.mywords_colors or {'#8ccbea', '#a4e57e', '#ffdb72', '#ff7272', '#ffb3ff', '#9999ff'}
 
-local word_group_mapping = {}
-local group_color_mapping = {}
+local group_word_color_mapping = {}
+local group_fifo = {}
 local avail_colors = colors
 
 -- clear highlight group to clear highlight of the word
 local function unhighlight_group(group)
+    -- unhighlight
     cmd(string.format('highlight clear %s', group))
-    for k, v in pairs(word_group_mapping) do
+
+    local color = group_word_color_mapping[group][2]
+
+    -- inser avail_colors
+    table.insert(avail_colors, color)
+    -- clear group record
+    group_word_color_mapping[group] = nil
+    -- remove group fifo
+    for k, v in pairs(group_fifo) do
         if v == group then
-            word_group_mapping[k] = nil
+            table.remove(group_fifo, k)
         end
     end
-
-    local color = group_color_mapping[group]
-    group_color_mapping[group] = nil
-    table.insert(avail_colors, 1, color)
 end
 
 local function find_avail_color()
     if #avail_colors ~= 0 then
         return avail_colors[1]
     else
-        for k, v in pairs(word_group_mapping) do
-            if word_group_mapping[k] ~= nil then
-                unhighlight_group(v)
-                return avail_colors[1]
-            end
-        end
+        local hl_group = group_fifo[1]
+        unhighlight_group(hl_group)
+        return avail_colors[1]
+    end
+end
+
+local function debug_helper_print_table(t)
+    for i=1, #t do
+        print(i)
+        print(t[i])
     end
 end
 
@@ -38,7 +47,7 @@ end
 local function highlight_word(word)
     local hl_group = 'mywords_'..word
 
-    if word_group_mapping[word] ~= nil then
+    if group_word_color_mapping[hl_group] ~= nil then
         unhighlight_group(hl_group)
     else
         local color = find_avail_color()
@@ -47,8 +56,9 @@ local function highlight_word(word)
             cmd(string.format('highlight %s guibg=%s', hl_group, color))
             local id = fn.matchadd(hl_group, string.format([[\<%s\>]], word), 11)
 
-            word_group_mapping[word] = hl_group
-            group_color_mapping[hl_group] = color
+            group_word_color_mapping[hl_group] = {word, color}
+            table.insert(group_fifo, hl_group)
+
             for i, c in pairs(avail_colors) do
                 if c == color then 
                     table.remove(avail_colors, i)
@@ -60,8 +70,9 @@ end
 
 -- unhighlight all words
 local function uhl_all()
-    for k, v in pairs(word_group_mapping) do
-        unhighlight_group(v)
+    for k, v in pairs(group_word_color_mapping) do
+        local hl_group = k
+        unhighlight_group(hl_group)
     end
 end
 
